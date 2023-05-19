8e5eee95aa5a33b9b048c8e8daac2d57154e7ac0
BLimaM4I <geral@m4i.pt>
2023-05-19 15:11:42 +0100

    Add recyclerView

--- app/src/main/java/m4i/blima/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/m4i/blima/a7minutesworkout/ExerciseActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import m4i.blima.a7minutesworkout.databinding.ActivityExerciseBinding
 import java.util.Locale
 
@@ -26,6 +27,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var tts: TextToSpeech? = null
     private var player: MediaPlayer? = null
+
+    private var exerciseAdapter: ExerciseStatusAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +50,14 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         }
 
         setupRestView()
+        setupExerciseStatusRecyclerView()
+    }
+
+    private fun setupExerciseStatusRecyclerView() {
+        binding?.rvExerciseStatus?.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        exerciseAdapter = ExerciseStatusAdapter(exerciseList!!)
+        binding?.rvExerciseStatus?.adapter = exerciseAdapter
     }
 
     private fun setupRestView() {
---
+++ app/src/main/java/m4i/blima/a7minutesworkout/ExerciseStatusAdapter.kt
@@ -0,0 +1,34 @@
+package m4i.blima.a7minutesworkout
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import m4i.blima.a7minutesworkout.databinding.ItemExerciseStatusBinding
+
+class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>) :
+    RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder>() {
+
+    inner class ViewHolder(binding: ItemExerciseStatusBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val tvItem = binding.tvItem
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemExerciseStatusBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model: ExerciseModel = items[position]
+        holder.tvItem.text = model.getId().toString()
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
