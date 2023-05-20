bf06be8767e6b63a3d0ad311270a7d354938bb1e
BLimaM4I <geral@m4i.pt>
2023-05-21 00:11:51 +0100

    Add History Activity

--- app/src/main/java/m4i/blima/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/m4i/blima/a7minutesworkout/HistoryActivity.kt
@@ -1,11 +1,25 @@
 package m4i.blima.a7minutesworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import m4i.blima.a7minutesworkout.databinding.ActivityHistoryBinding
 
 class HistoryActivity : AppCompatActivity() {
+
+    private var binding: ActivityHistoryBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarHistoryActivity)
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.title = "HISTORY"
+        }
+        binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
